7e0fb304f59d43632427dab4e2f839947ce044f9
fundwit <fundwit@qq.com>
2019-06-04 23:37:00 +0800

    fix the ci/cd build script for only master branch deploy.
    Signed-off-by: fundwit <fundwit@qq.com>

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -18,3 +18,19 @@ export IMAGE_NAME=registry.cn-beijing.aliyuncs.com/fundwit/shikra:$BUILD_VERSION
 echo "build.version=$BUILD_VERSION" > docker-build/service/build.properties
 
 docker build -t $IMAGE_NAME docker-build
+
+if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    docker login registry.cn-beijing.aliyuncs.com -u $DOCKER_ACCOUNT -p $DOCKER_PASSWORD
+    docker push $IMAGE_NAME
+
+    mkdir -p kube-build
+    envsubst < .cicd/kube/shikra.yaml > kube-build/shikra.yaml
+
+    ssh -p $DEPLOY_PORT -i ~/.ssh/deploy_key -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_TARGET "mkdir -p /opt/cicd/shikra/$BUILD_VERSION"
+    scp -P $DEPLOY_PORT -i ~/.ssh/deploy_key kube-build/shikra.yaml $DEPLOY_USER@$DEPLOY_TARGET:/opt/cicd/shikra/$BUILD_VERSION/shikra.yaml
+    ssh -p $DEPLOY_PORT -i ~/.ssh/deploy_key $DEPLOY_USER@$DEPLOY_TARGET "kubectl apply -f /opt/cicd/shikra/$BUILD_VERSION/shikra.yaml"
+
+    # TODO: test this release is running
+else
+    echo "this build is not for master branch, skip deploy!"
+fi
